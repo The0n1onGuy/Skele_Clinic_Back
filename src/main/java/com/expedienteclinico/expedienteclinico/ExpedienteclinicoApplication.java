7b4167476dc448147713db5a9139525ab9bc2b35
Lex Castillo <ing.lexcastillo@gmail.com>
2026-01-29 08:55:35 -0500

    Initial Commit

---
+++ src/main/java/com/expedienteclinico/expedienteclinico/ExpedienteclinicoApplication.java
@@ -0,0 +1,13 @@
+package com.expedienteclinico.expedienteclinico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpedienteclinicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpedienteclinicoApplication.class, args);
+	}
+
+}
