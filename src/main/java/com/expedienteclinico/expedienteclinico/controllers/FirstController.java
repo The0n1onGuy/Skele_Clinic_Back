6b2a651c748cce7535be8b9aa2a69c11ab44a593
0bones0 <nataliacarrillo418@gmail.com>
2026-02-10 11:05:24 -0500

    Prueba de funcionamiento

--- src/main/java/com/expedienteclinico/expedienteclinico/controllers/FirstController.java
+++ src/main/java/com/expedienteclinico/expedienteclinico/controllers/FirstController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//Esto es una prueba
 @CrossOrigin( origins = "*" , maxAge = 3600 )
 @RestController
 @RequestMapping( "/first-controller/" )
