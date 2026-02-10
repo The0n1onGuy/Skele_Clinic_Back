6eb9ab3a39bcb84a2aac1bd6b715203c0a1b2aca
Lex <ing.lexcastillo@gmail.com>
2026-02-10 09:31:09 -0500

    Actualización de .gitignore

--- src/main/java/com/expedienteclinico/expedienteclinico/models/StatusModel.java
+++ src/main/java/com/expedienteclinico/expedienteclinico/models/StatusModel.java
@@ -1,4 +1,23 @@
 package com.expedienteclinico.expedienteclinico.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Table;
+
+import javax.persistence.*;
+
+@Entity
+@Table( appliesTo = "estatus" )
+@Getter
+
 public class StatusModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(nullable = false)
+    private Long id;
+
+    private String statusName ;
+
 }
