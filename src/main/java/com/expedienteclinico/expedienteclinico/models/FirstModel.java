58a4df013c8a9417797c1211c9cef7d8bbf39f8c
Lex <ing.lexcastillo@gmail.com>
2026-02-10 08:35:54 -0500

    Init commit.

---
+++ src/main/java/com/expedienteclinico/expedienteclinico/models/FirstModel.java
@@ -0,0 +1,25 @@
+package com.expedienteclinico.expedienteclinico.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Table;
+
+import javax.persistence.*;
+
+@Entity
+@Table( appliesTo = "first_model" )
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class FirstModel {
+    @Id
+    @GeneratedValue( strategy = GenerationType.SEQUENCE )
+    @Column( nullable = false )
+    private Long id ;
+    private String nombre ;
+    private String apellido ;
+
+}
---
+++ src/main/java/com/expedienteclinico/expedienteclinico/models/StatusModel.java
@@ -0,0 +1,4 @@
+package com.expedienteclinico.expedienteclinico.models;
+
+public class StatusModel {
+}
