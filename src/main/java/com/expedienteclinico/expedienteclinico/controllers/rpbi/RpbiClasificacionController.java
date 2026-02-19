95be9436b9ebe5c6216dccca8c0970a38dc7ceed
Alex Caamal Solís <alexcaamal471@gmail.com>
2026-02-19 13:14:15 -0500

    Avance en los  modelos Clasifiacion, Envase

--- src/main/java/com/expedienteclinico/expedienteclinico/controllers/rpbi/RpbiClasificacionController.java
+++ src/main/java/com/expedienteclinico/expedienteclinico/controllers/rpbi/RpbiClasificacionController.java
@@ -1,12 +1,15 @@
 package com.expedienteclinico.expedienteclinico.controllers.rpbi;
 
 import com.expedienteclinico.expedienteclinico.beans.rpbi.RpbiClasificacionObject;
+import com.expedienteclinico.expedienteclinico.payload.response.ResponseFactory;
 import com.expedienteclinico.expedienteclinico.services.rpbi.RpbiClasificacionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/rpbi/clasificacion") // ruta base del módulo
@@ -17,9 +20,11 @@ public class RpbiClasificacionController {
     private RpbiClasificacionService service;
 
     @GetMapping("/all")
-    public ResponseEntity<List<RpbiClasificacionObject>> getAll() {
-        // llamada al servicio y retorno de un HTTP 200 (el ok) con la lista
-        return ResponseEntity.ok(service.getAllClasificaciones());
-                            // ↑ Esta cosa
+    public ResponseEntity<Map<String, Object>> getAll() {
+
+        List<RpbiClasificacionObject> lista = service.getAllClasificaciones();
+        return new  ResponseEntity<Map<String, Object>>( ResponseFactory.getSuccessOnGetAllResponse(lista) , HttpStatus.OK );
+
+
     }
 }
--- src/main/java/com/expedienteclinico/expedienteclinico/services/rpbi/RpbiEnvaseService.java
+++ src/main/java/com/expedienteclinico/expedienteclinico/services/rpbi/RpbiEnvaseService.java
@@ -1,7 +1,41 @@
 package com.expedienteclinico.expedienteclinico.services.rpbi;
 
+import com.expedienteclinico.expedienteclinico.beans.rpbi.RpbiClasificacionObject;
+import com.expedienteclinico.expedienteclinico.models.rpbi.RpbiEnvaseModel;
+import com.expedienteclinico.expedienteclinico.repositories.rpbi.IRpbiEnvaseRepository;
+import com.expedienteclinico.expedienteclinico.beans.rpbi.RpbiEnvaseObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class RpbiEnvaseService {
+
+@Autowired
+    private IRpbiEnvaseRepository repository;
+
+public List<RpbiEnvaseObject> getAll(){
+
+    List<RpbiEnvaseModel> entidades = repository.findAll();
+    List<RpbiEnvaseObject> dtos = new ArrayList<>();
+
+    for (RpbiEnvaseModel entidad : entidades){
+
+        RpbiEnvaseObject dto = new RpbiEnvaseObject();
+
+        dto.setId(entidad.getId());
+        dto.setNombre(entidad.getNombre());
+        dto.setDescripcion(entidad.getDescripcion());
+        if(entidad.getEstatus() != null ){
+
+            dto.setEstatusId(entidad.getEstatus().getId());
+            dto.setEstatusNombre(entidad.getEstatus().getStatusName());
+        }
+        dtos.add(dto);
+    }
+return dtos;
+}
+
 }
