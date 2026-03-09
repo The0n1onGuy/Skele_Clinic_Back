d02597e0756e83fbd1e818e780eef65d97d09d1f
Alex Caamal Solís <alexcaamal471@gmail.com>
2026-03-09 08:19:38 -0500

    Práctica pruebas unitarias

---
+++ src/main/java/com/expedienteclinico/expedienteclinico/models/Banco.java
@@ -0,0 +1,39 @@
+package com.expedienteclinico.expedienteclinico.models;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class Banco {
+
+    private String nombre;
+    private List<Cuenta> cuentas;
+
+    public Banco() {
+
+        cuentas = new ArrayList<>();
+
+    }
+
+    public void transferir(Cuenta origen, Cuenta destino, BigDecimal monto){
+
+        origen.debito(monto);
+        destino.credito(monto);
+
+
+    }
+
+
+    public void addCuenta(Cuenta cuenta) {
+
+        cuentas.add(cuenta);
+        cuenta.setBanco(this);
+    }
+
+}
+
--- src/test/java/com/expedienteclinico/expedienteclinico/models/CuentaTest.java
+++ src/test/java/com/expedienteclinico/expedienteclinico/models/CuentaTest.java
@@ -1,9 +1,11 @@
 package com.expedienteclinico.expedienteclinico.models;
 
+import com.expedienteclinico.expedienteclinico.exceptions.DineroInsuficienteException;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+import org.springframework.test.context.TestPropertySource;
 
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -49,24 +51,126 @@ class CuentaTest {
 
     }
 
+//    @Test
+//    @DisplayName("Ensure that two temporary directories with same files names and content have same hash")
+//    void hashTwoDynamicDirectoryWhichHaveSameContent(@TempDir Path tempDir, @TempDir Path tempDir2) throws IOException {
+//
+//        Path file1 = tempDir.resolve("myfile.txt");
+//
+//        List<String> input = Arrays.asList("input1", "input2", "input3");
+//        Files.write(file1, input);
+//
+//        assertTrue(Files.exists(file1), "File should exist");
+//
+//        Path file2 = tempDir2.resolve("myfile.txt");
+//
+//        Files.write(file2, input);
+//        assertTrue(Files.exists(file2), "File should exist");
+//
+//    }
+
     @Test
-    @DisplayName("Ensure that two temporary directories with same files names and content have same hash")
-    void hashTwoDynamicDirectoryWhichHaveSameContent(@TempDir Path tempDir, @TempDir Path tempDir2) throws IOException {
+    public void testDebidotCuenta (){
 
-        Path file1 = tempDir.resolve("myfile.txt");
-
-        List<String> input = Arrays.asList("input1", "input2", "input3");
-        Files.write(file1, input);
-
-        assertTrue(Files.exists(file1), "File should exist");
-
-        Path file2 = tempDir2.resolve("myfile.txt");
-
-        Files.write(file2, input);
-        assertTrue(Files.exists(file2), "File should exist");
+        Cuenta cuenta = new Cuenta("Alex", new BigDecimal( "1000.12345" ) ) ;
+        cuenta.debito(new BigDecimal( 100 )) ;
+        assertNotNull(cuenta.getSaldo() );
+        assertEquals( 900 , cuenta.getSaldo().intValue() ) ;
+        assertEquals( "900.12345" , cuenta.getSaldo().toPlainString() ) ;
 
     }
 
+    @Test
+    public void testCreditoCuenta (){
 
+        Cuenta cuenta = new Cuenta("Alex", new BigDecimal( "1000.12345" ) ) ;
+        cuenta.credito(new BigDecimal( 100 )); ;
+        assertNotNull(cuenta.getSaldo() );
+        assertEquals( 1100 , cuenta.getSaldo().intValue() ) ;
+        assertEquals( "1100.12345" , cuenta.getSaldo().toPlainString() ) ;
+
+    }
+
+    @Test
+    void testDineroInsuficienteExceptionCuenta(){
+
+        Cuenta cuenta = new Cuenta("Alex", new BigDecimal( "1000.12345" ) ) ;
+
+        Exception exception = assertThrows( DineroInsuficienteException.class , () ->{
+
+            cuenta.debito(new BigDecimal( "1500" )) ;
+
+        });
+
+        String actual = exception.getMessage();
+        String esperado = "C jodido.";
+
+        assertEquals( esperado , actual ) ;
+
+    }
+
+    @Test
+    void tesTransferirDineroCuentas(){
+
+        Cuenta cuenta1 = new Cuenta("Alex", new BigDecimal( "1000.01" ) ) ;
+        Cuenta cuenta2 = new Cuenta("Darikson", new BigDecimal( "1500.01" ) ) ;
+
+        Banco banco = new Banco();
+            banco.setNombre("Mercado Pago");
+            banco.transferir( cuenta2, cuenta1, new BigDecimal("500" ) );
+
+        assertEquals( "1000.01", cuenta2.getSaldo().toPlainString() ) ;
+        assertEquals( "1500.01", cuenta1.getSaldo().toPlainString() ) ;
+    }
+
+    @Test
+    void testRelacionBancoCuentas() {
+
+        Cuenta cuenta1 = new Cuenta("Alex", new BigDecimal("1000.01"));
+        Cuenta cuenta2 = new Cuenta("Darikson", new BigDecimal("1500.01"));
+
+        Banco banco = new Banco();
+
+        banco.addCuenta(cuenta1);
+        banco.addCuenta(cuenta2);
+
+
+        banco.setNombre("Mercado Pago");
+        banco.transferir(cuenta2, cuenta1, new BigDecimal("500"));
+
+        assertEquals("1000.01", cuenta2.getSaldo().toPlainString());
+        assertEquals("1500.01", cuenta1.getSaldo().toPlainString());
+
+        assertEquals(2, banco.getCuentas().size());
+
+        assertEquals("Mercado Pago", cuenta1.getBanco().getNombre());
+
+        assertEquals("Darikson", banco.getCuentas().stream()
+                .filter(c -> c.getPersona().equals("Darikson"))
+                .findFirst()
+                .get().getPersona()
+        );
+
+        assertTrue(banco.getCuentas().stream()
+                .filter(c -> c.getPersona().equals("Darikson"))
+                .findFirst().isPresent());
+
+        assertTrue( banco.getCuentas().stream()
+                .anyMatch(c -> c.getPersona().equals("Alex"))
+        );
+
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
