785616e7dcc5f16eb4952d86d9651166b9ce6d2e
Lex <ing.lexcastillo@gmail.com>
2026-03-24 09:56:33 -0500

    Ejercicios TESTeo

---
+++ src/main/java/com/expedienteclinico/expedienteclinico/models/Banco.java
@@ -0,0 +1,33 @@
+package com.expedienteclinico.expedienteclinico.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class Banco {
+
+    private String nombre ;
+    private List< Cuenta > cuentas ;
+
+    public Banco() {
+        cuentas = new ArrayList<>() ;
+    }
+
+    public void transferir( Cuenta origen , Cuenta destino , BigDecimal monto ) {
+
+        origen.debito( monto ) ;
+        destino.credito( monto ) ;
+
+    }
+
+    public void addCuenta( Cuenta cuenta ) {
+        cuentas.add( cuenta ) ;
+        cuenta.setBanco( this ) ;
+    }
+
+}
